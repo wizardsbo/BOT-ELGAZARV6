b16fc73c8fd7f4f5c1d1ee56e428541931f672f1
wizardsbo <136733115+wizardsbo@users.noreply.github.com>
2023-06-15 19:14:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['966565089504'] //ur owner number
+global.ownername = "Wizards 🧙🏻‍♂️" //ur owner name
+global.ytname = "YT: Wizards" //ur yt chanel name
+global.socialm = "GitHub: Wizards" //ur github or insta name
+global.Region = "Asia, Saudi, Mecca" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
-global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.ownernomer = "966565089504" //ur number
+global.premium = ['966565089504'] //ur premium number
+global.botname = '🧙🏻‍♂️Wizards Bot🧙🏻‍♂️ ' //ur bot name
+global.linkz = "https://youtube.com"  //your theme url which will be displayed on whatsapp
+global.websitex = "https://telegra.ph" //ur website to be displayed
+global.botscript = 'https://youtube.com' //script link
+global.themeemoji = "🧙🏻‍♂️" //ur theme emoji
+global.packname = "⍣وزرد يروحي⍣" //ur sticker watermark packname
+global.author = "⍣966565089504⍣" //ur sticker watermark author
+global.wm = "🧙🏻‍♂️Wizards ᗷOT🧙🏻‍♂️." //ur watermark
 
 // Other
 global.sessionName = 'session'
